2a295bc1446994633e78e84465093dc0bbaea5df
Nivardo <nivardo00@gmail.com>
2020-10-26 11:41:50 -0300

    DEV - tests and swagger

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_script import Manager
-
+from flask_restplus import Api
 
 app = Flask(__name__)
+api = Api(app=app)
 CORS(app, resources={"*": {"origins": "*"}})
 app.config.from_object("config")
 jwt = JWTManager(app)
 
 manager = Manager(app)
+
+name_space = api.namespace("main", description="Main APIs")
+
+# ---------IMPORTS--------------#
